31242449a384f46fc384490bc64f4edbbe2d6fbd
Om Tiwary <67217318+KHYZYL@users.noreply.github.com>
2026-09-22 07:34:06 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'atten-cache-v1';
+const urlsToCache = [
+  '/New_atten/',
+  '/New_atten/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
